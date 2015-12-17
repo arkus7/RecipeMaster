de61f9f4ae63f1b9b0349578c1b68f0503fb360b
Arkadiusz Żmudzin <arkus777@gmail.com>
2015-12-17 20:56:44 +0100

    add new floating action buttons

--- app/src/main/java/pl/nstrefa/arkus/recipemaster/MainActivity.java
+++ app/src/main/java/pl/nstrefa/arkus/recipemaster/MainActivity.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +31,8 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginBehavior;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -133,26 +136,23 @@ public class MainActivity extends AppCompatActivity {
 
         //fb logged as
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final FloatingActionButton fb = (FloatingActionButton) findViewById(R.id.fb);
-        final FloatingActionButton recipe = (FloatingActionButton) findViewById(R.id.getrecipe);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionsMenu actionButton = (FloatingActionsMenu) findViewById(R.id.actionMenu);
+        FloatingActionButton fb = (FloatingActionButton) findViewById(R.id.facebook);
+        FloatingActionButton recipe = (FloatingActionButton) findViewById(R.id.getRecipe);
+
+        actionButton.setOnFloatingActionsMenuUpdateListener(new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
+
             @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                switch (fb.getVisibility()) {
-                    case View.VISIBLE :
-                        fb.setVisibility(View.GONE);
-                        recipe.setVisibility(View.GONE);
-                        break;
-                    case View.GONE:
-                        fb.setVisibility(View.VISIBLE);
-                        recipe.setVisibility(View.VISIBLE);
-                        break;
-                }
+            public void onMenuExpanded() {
+
+            }
+
+            @Override
+            public void onMenuCollapsed() {
+
             }
         });
+
         ImageButton pizzaImage = (ImageButton) findViewById(R.id.pizzaImage);
 
         fb.setOnClickListener(new View.OnClickListener() {
